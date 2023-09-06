b9bdc00ae9aa6e01764bb82ab4a8ccde170b5954
Paul De Ulloa <pauld143@gmail.com>
2023-09-05 22:50:18 -0700

    App.js axios integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import PokemonFetch from "./components/PokemonFetch";
+import PokemonName from "./components/PokemonName";
+import axios from 'axios';
+
 
 function App() {
+  const [pokemonName, setPokemonName] = useState([]);
+
+  const fetchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=807&offset=0`)
+      .then((res) => {
+        console.log(res);
+        setPokemonName(res.data.results);
+    })
+    .catch((err) => console.log(err));
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3 d-flex flex-column align-items-center">
+      <PokemonFetch fetchPokemon={fetchPokemon} />
+      <PokemonName pokemonName={pokemonName} />
     </div>
   );
-}
+};
 
 export default App;
